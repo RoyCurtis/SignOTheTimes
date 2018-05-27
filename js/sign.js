3dfafed37420819260535c1c830f94941c5829c4
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2018-05-27 16:24:08 +0100

    Initial commit

---
+++ js/sign.js
@@ -0,0 +1,6 @@
+"use strict";
+class Sign {
+    static main() {
+    }
+}
+//# sourceMappingURL=sign.js.map
---
+++ js/sign.ts
@@ -0,0 +1,9 @@
+/** Sign '☮' The Times. By Roy Curtis, MIT license, 2018 */
+
+class Sign
+{
+    public static main()
+    {
+
+    }
+}
